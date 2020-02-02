a880a8947d136d81845ad5a091c326b0df4dfe12
devJ3rry <dev.j3rry@gmail.com>
2020-02-03 02:03:54 +0900

    Posts 등록,수정,조회 및 테스트 클래스 구현

--- src/main/java/com/j3rry/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/j3rry/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class posts {
+public class Posts {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,9 +29,16 @@ public class posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
 
 /*
 * Posts 클래스는 실제 DB의 테이블과 매칭될 클래스이며 보통 Entity 클래스 라고도 한다.
 * JPA를 사용하면 DB 데이터에 작업할 경우 실제 쿼리를 날리기 보다 이 Entity 클래스의 수정을 통해 작업한다.
+* Entity 클래스에서는 절대 Setter 메소드를 만들지 않는다.
+* 대신 빌터 패턴을 이용해 값을 채워 DB에 삽입한다.
 * */
