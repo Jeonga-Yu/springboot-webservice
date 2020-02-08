8b4df65c32476cff63c486c0f52076af158547b3
devJ3rry <dev.j3rry@gmail.com>
2020-02-08 23:28:05 +0900

    4.4 전체 조회 화면 만들기

--- src/main/java/com/j3rry/book/springboot/web/IndexController.java
+++ src/main/java/com/j3rry/book/springboot/web/IndexController.java
@@ -1,14 +1,19 @@
 package com.j3rry.book.springboot.web;
 
+import com.j3rry.book.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
