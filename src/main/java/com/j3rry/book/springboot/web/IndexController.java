e39ef56df27353d3d968a00802b4a54bb2a791d0
devJ3rry <dev.j3rry@gmail.com>
2020-02-13 21:32:16 +0900

    IndexControllerTest 에러 수정
    index.mustache에 header, footer에 해당하는 내용이 포함되어있었음(html태그 선언이 2중으)로
    해결완료!

--- src/main/java/com/j3rry/book/springboot/web/IndexController.java
+++ src/main/java/com/j3rry/book/springboot/web/IndexController.java
@@ -15,22 +15,19 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if(user != null) {
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
     }
 
-    @GetMapping("posts/save")
+    @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
